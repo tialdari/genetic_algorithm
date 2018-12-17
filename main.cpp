b09855b4bd959c90d3d71819d33c74b9e5558d26
Asia <asialuk98@gmail.com>
2018-12-17 14:29:07 +0100

    fix getFileData method

--- main.cpp
+++ main.cpp
@@ -14,82 +14,22 @@
 
 using namespace std;
 
-static float** getFileData(string fileName, int &externalSize);
+static float** getFileData(string fileName, float &externalSize, float &maxVolume);
 static string readFromFile(string fileName);
 static vector<string> splitString(string dataString);
-static float** createParametersArray(vector<string> floatsString, int &externalSize);
+static float** createParametersArray(vector<string> floatsString, float &externalSize, float &maxVolume);
 static void getInputData(string &fileName, int &popSize, float &time);
 
 
 int main(){
-/*
+
   srand (time(NULL));
+  float solutionSize = 0;
+  float maxVolume = 0;
 
-  int paramSize = 2;
-  int solutionSize = 15;
-  float** pParameters = new float*[solutionSize];
+  float** pParameters = getFileData("test2.txt", solutionSize, maxVolume);
 
-  pParameters[0] = new float[paramSize];
-  pParameters[0][0] = 0.125126;
-  pParameters[0][1] =  56.358531;
-
-  pParameters[1] = new float[paramSize];
-  pParameters[1][0] = 19.330424;
-  pParameters[1][1] = 80.874050;
-
-  pParameters[2] = new float[paramSize];
-  pParameters[2][0] = 58.500931;
-  pParameters[2][1] = 47.987304;
-
-  pParameters[3] = new float[paramSize];
-  pParameters[3][0] = 35.029145;
-  pParameters[3][1] = 89.596240;
-
-  pParameters[4] = new float[paramSize];
-  pParameters[4][0] = 82.284005;
-  pParameters[4][1] = 74.660482;
-
-  pParameters[5] = new float[paramSize];
-  pParameters[5][0] = 17.410810 ;
-  pParameters[5][1] = 85.894345;
-
-  pParameters[6] = new float[paramSize];
-  pParameters[6][0] = 71.050142;
-  pParameters[6][1] = 51.353496;
-
-  pParameters[7] = new float[paramSize];
-  pParameters[7][0] = 30.399487;
-  pParameters[7][1] = 1.498459;
-
-  pParameters[8] = new float[paramSize];
-  pParameters[8][0] = 9.140294;
-  pParameters[8][1] = 36.445204;
-
-  pParameters[9] = new float[paramSize];
-  pParameters[9][0] = 14.731285;
-  pParameters[9][1] = 16.589862;
-
-  pParameters[10] = new float[paramSize];
-  pParameters[10][0] = 98.852504 ;
-  pParameters[10][1] = 44.569231;
-
-  pParameters[11] = new float[paramSize];
-  pParameters[11][0] = 11.908322;
-  pParameters[11][1] = 0.466933;
-
-  pParameters[12] = new float[paramSize];
-  pParameters[12][0] = 0.891140;
-  pParameters[12][1] = 37.788018;
-
-  pParameters[13] = new float[paramSize];
-  pParameters[13][0] = 53.166295;
-  pParameters[13][1] = 57.118442;
-
-  pParameters[14] = new float[paramSize];
-  pParameters[14][0] = 60.176397;
-  pParameters[14][1] = 60.716575;
-
-  CKnapsackProblem* cKnapsackProblem = new CKnapsackProblem(solutionSize, pParameters, 375.0);
+  CKnapsackProblem* cKnapsackProblem = new CKnapsackProblem(solutionSize, pParameters, maxVolume);
   cout << cKnapsackProblem -> toString();
 
   CGeneticAlgorithm cGeneticAlgorithm(cKnapsackProblem);
@@ -105,26 +45,14 @@ int main(){
 
   delete cKnapsackProblem;
   return 0;
-*/
-
-  int size = 0;
-  float** fileData = getFileData("test1.txt", size);
-
-
-  for(int i = 0; i < size; i ++){
-    for(int j = 0; j < 2; j++){
-      cout << (2 * i + j) << ". " << fileData[i][j] << " ";
-    }
-  }
-
 
 }
 
-static float** getFileData(string fileName, int &externalSize){
+static float** getFileData(string fileName, float &externalSize, float &maxVolume){
 
   string fileString = readFromFile(fileName);
   vector<string> splitStringVec = splitString(fileString);
-  float** resultArray = createParametersArray(splitStringVec, externalSize);
+  float** resultArray = createParametersArray(splitStringVec, externalSize, maxVolume);
   return resultArray;
 }
 
@@ -156,30 +84,26 @@ static vector<string> splitString(string dataString){
   return stringFloatData;
 }
 
-static float** createParametersArray(vector<string> floatsString, int &externalSize){
+static float** createParametersArray(vector<string> floatsString, float &externalSize, float &maxVolume){
 
   int size = floatsString.size();
-  externalSize = (size / 2) - 1;
-  //cout << "size: " << externalSize << endl;
-
-  float** floatArr = new float*[size/2 - 1];
   const int firstIndex = 1;
   const int parametersNum = 2;
+  float** floatArr = new float*[size/2 - firstIndex];
+  int currentIndex = 0;
+
+  externalSize = strtof(floatsString[currentIndex++].c_str(),0);
+  maxVolume = strtof(floatsString[currentIndex++].c_str(),0);
 
   for(int i = 0; i < size/2 - firstIndex; i ++){
     floatArr[i] = new float[parametersNum];
     for(int j = 0; j < parametersNum; j++){
-      floatArr[i][j] = strtof(floatsString[2*(i + firstIndex) + j].c_str(),0);
+      floatArr[i][j] = strtof(floatsString[currentIndex].c_str(),0);
+      currentIndex++;
     }
   }
   return floatArr;
 }
 
-
 static void getInputData(string &fileName, int &popSize, float &time){
 }
-
-
-//check potential memory leaks
-//enable the user to set all the parameters
-//make the algorithm compatible with any problem
